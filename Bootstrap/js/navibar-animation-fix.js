c1dd5c40e9539e03eede4acc01b0987228c10b91
MaiconReis <jcr.maicon@gmail.com>
2022-01-13 20:38:07 -0300

    first commit

---
+++ Bootstrap/js/navibar-animation-fix.js
@@ -0,0 +1,6 @@
+$('#collapse-navbar').on('show.bs.collapse', function() {
+    $('.topcasafina-banner').css('transform','translate(-50%, -50%');
+} );
+$('#collapse-navbar').on('hide.bs.collapse', function() {
+    $('.topcasafina-banner').css('transform','translate(-50%,-50%');
+});
